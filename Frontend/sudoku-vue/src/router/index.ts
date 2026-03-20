e7a39162f2c42345f6c8df01e0399f72433fbf4e
thienphuocho <thienphuocho@gmail.com>
2026-03-20 09:57:07 -0700

    Add back route for welcome page

--- Frontend/sudoku-vue/src/router/index.ts
+++ Frontend/sudoku-vue/src/router/index.ts
@@ -8,7 +8,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-    },
+    }, {
+      path: '/welcome',
+      name: 'welcome',
+      component: () => import('../components/TheWelcome.vue')
+    }
+    
   ],
 })
 
